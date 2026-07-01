f6c602cde22d7275581b14eba9b2153ce63c64bc
Maira Taimoor <mimran.msee16seecs@seecs.edu.pk>
2026-06-30 23:56:20 -0400

    03_data_validation.sql

--- sql/03_data_validation.sql
+++ sql/03_data_validation.sql
@@ -8,8 +8,23 @@ SELECT COUNT(*) FROM olist_order_payments_dataset;
 SELECT * FROM olist_orders_dataset WHERE order_id IS NULL;
 
 -- Orphan check
+-- Orders without customers
 SELECT o.*
 FROM olist_orders_dataset o
 LEFT JOIN olist_customers_dataset c
 ON o.customer_id = c.customer_id
 WHERE c.customer_id IS NULL;
+
+--Order items without orders
+SELECT oi.*
+FROM olist_order_items_dataset oi
+LEFT JOIN olist_orders_dataset o
+ON oi.order_id = o.order_id
+WHERE o.order_id IS NULL;
+
+-- Payments without orders
+SELECT p.*
+FROM olist_order_payments_dataset p
+LEFT JOIN olist_orders_dataset o
+ON p.order_id = o.order_id
+WHERE o.order_id IS NULL;
